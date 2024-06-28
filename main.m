62715179500687bd483ca6eb7db75565a290f347
Kyra Ruiz <kyramruiz001@gmail.com>
2024-06-28 12:23:57 -0700

    added a plotting function for the stokeslet

--- main.m
+++ main.m
@@ -19,3 +19,12 @@ stks = getStokesletPositions(rho,geometry_type,system,U0);
 
 %% Get the flow over the space
 [Uflowx,Uflowy] = calculateFlowGrid_serial2(stks,iS,x,y,eps_reg);
+
+%% Plots output the flowfield
+n=5; % Plot coarseness
+Umag = sqrt(Uflowx.^2 + Uflowy.^2); % Get flow field magnitude
+imagesc(y,x,Umag) % Plot local flow strenght as a background
+hold on
+scatter(stks(:,2),stks(:,1),2,'r') % Plot the Stokeslets
+quiver(y(1:n:end),x(1:n:end),Uflowy(1:n:end,1:n:end),Uflowx(1:n:end,1:n:end),2,'Color','w') % Plot the vector field
+axis equal
