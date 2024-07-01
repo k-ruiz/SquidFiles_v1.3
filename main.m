78724156477746c7acfa720bb0ca3048ad9a0f3f
Kyra Ruiz <kyramruiz001@gmail.com>
2024-07-01 12:35:30 -0700

    added a clear and close command

--- main.m
+++ main.m
@@ -1,8 +1,9 @@
 % Title: Code to get the fluid flow from a set of Regularized Stokeslets.
 % Author: Stephen Williams.
 
-%close all
-%clear all %#ok<CLALL>
+clc
+close
+tic
 
 %% Add the function files need to run
 addpath('functions/')
@@ -28,3 +29,5 @@ hold on
 scatter(stks(:,2),stks(:,1),2,'r') % Plot the Stokeslets
 quiver(y(1:n:end),x(1:n:end),Uflowy(1:n:end,1:n:end),Uflowx(1:n:end,1:n:end),2,'Color','w') % Plot the vector field
 axis equal
+
+toc
