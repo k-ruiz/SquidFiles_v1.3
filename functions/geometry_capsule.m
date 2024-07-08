365c4b1a1d953588bf343fc23d78480d89beaff0
Kyra Ruiz <kyramruiz001@gmail.com>
2024-07-08 12:51:06 -0700

    introducing gaps in the capsule boundary. the only way i've found for the code to semi-work properly

--- functions/geometry_capsule.m
+++ functions/geometry_capsule.m
@@ -9,23 +9,25 @@
 function [caps_stks] = geometry_capsule(rho2,Pty2,Ltot2,side2,bott2,top2)
 
     % construction of the top boundary
-    phi = linspace(0,pi,floor(top2*rho2*pi)); % gets the angles to parameterise the surface of the top boundary
+    phi = linspace(0,pi,ceil(top2*rho2*pi)); % gets the angles to parameterise the surface of the top boundary
 
-    wallB = [cos(phi);sin(phi);zeros(floor(top2*rho2*pi),1)']'; % gets the coordinates of the half-circle
+    wallB = [cos(phi);sin(phi);zeros(ceil(top2*rho2*pi),1)']'; % gets the coordinates of the half-circle
     wallB = floor(top2)*wallB + [0,side2,0]; % shifts the half-circle to the top of the squid
+    %wallB = wallB(1:end);
 
     % construction of the bottom boundaries
-    wallD = [linspace(bott2,top2-1,ceil(top2*rho2));ceil(Pty2-Ltot2)*ones(ceil(top2*rho2),1)';ones(ceil(top2*rho2),1)']';
+    wallD = [linspace(bott2,top2-1,floor(top2*rho2));floor(Pty2-Ltot2)*ones(floor(top2*rho2),1)';ones(floor(top2*rho2),1)']';
+    wallD = wallD(2:end-1,:); 
 
     wallE = wallD;
     wallE(:,1) = -wallE(:,1); % reflect across the x axis
-    %wallE = wallE(2:end-1,:); 
 
     % construction of the side boundaries 
-    wallA = [floor(top2)*ones(ceil(2*side2*rho2),1)';linspace(Pty2-Ltot2,side2,ceil(2*side2*rho2));zeros(ceil(2*side2*rho2),1)']';
+    wallA = [floor(top2)*ones(ceil(2*side2*rho2),1)';linspace(Pty2-Ltot2,side2-1,ceil(2*side2*rho2));zeros(ceil(2*side2*rho2),1)']';
+    %wallA = wallA(1:end);
 
     wallC = wallA;
-    wallC(:,1) = -wallC(:,1); % reflect accorss the x axis
+    wallC(:,1) = -wallC(:,1); % reflect acorss the x axis
 
     % combining all the arrays
     caps_stks = [wallA;wallB;wallC;wallD;wallE]; % combines all the boundaries
@@ -34,6 +36,3 @@ end
 % i have a question... for god... why?
 
 % this code has set me on an emotional journey of edurance
-
-% everyday I question why this things doesn't work. then i realize that i am
-% the problem
